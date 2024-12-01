b149bfea250c52d5cd8987b769330cbc1ca3e216
julioborgesigt <julio.aparecido3@email.com>
2024-11-30 22:48:20 -0300

    json

--- clientes.js
+++ clientes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../server');
+const pool = require('./server');
 
 // Listar todos os clientes
 router.get('/', async (req, res) => {
--- users.js
+++ users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../server');
+const pool = require('./server');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
